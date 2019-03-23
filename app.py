7a87d35b76dcae072999b64ee2f6799b31d3a6e2
TaniaGameiro <35870773+TaniaGameiro@users.noreply.github.com>
2019-03-22 23:15:01 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from sklearn.externals import joblib
 import os
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__, static_url_path='/static/')
 
@@ -13,13 +15,13 @@ def form():
 @app.route('/predict_price', methods=['POST', 'GET'])
 def predict_price():
     # get the parameters
-    Year = int(request.form['Year'])
-    Make = str(request.form['Make'])
-    Model = str(request.form['Model'])
-    Transmission = str(request.form['Transmisson'])
+    Year=int(request.form['Car_Year'])
+    Make=str(request.form['Car_Make'])
+    Model=str(request.form['Car_Model'])
+    Transmission=str(request.form['Car_Transmission'])
 
- # load the X_columns file
-    X_columns = joblib.load('model/X_columns.joblib')
+    #load the X_columns file
+    X_columns = joblib.load('Model/X_columns.joblib')
     print(X_columns)
 
     # generate a dataframe with zeros
@@ -29,25 +31,22 @@ def predict_price():
     # change the dataframe according to the inputs
     df_prediction.at[0, 'Year'] = Year
     df_prediction.at[0, 'Make_'+str(Make)] = 1.0
-    df_prediction.at[0, 'Model'] = Model
-    df_prediction.at[0, 'Transmission_'+str(Transmisson)] = 1.0
+    df_prediction.at[0, 'Model'+str(Model)] = 1.0
+    df_prediction.at[0, 'Transmission_'+str(Transmission)] = 1.0
     print(df_prediction)
 
     # load the model and predict
-    model = joblib.load('model/model.joblib')
+    model = joblib.load('Model/model.joblib')
     prediction = model.predict(df_prediction.head(1).values)
-    predicted_time = prediction.round(1)[0]
+    predict_price = prediction.round(1)[0]
 
-
-return render_template('results.html',
-                           Year=int(Year),
+    return render_template('results.html', 
+                           Year=str(Year),
                            Make=str(Make),
                            Model=str(Model),
                            Transmission=str(Transmission),
-                           predicted_price=int(predicted_time)
+                           predict_price=int(predict_price)
                            )
-
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
